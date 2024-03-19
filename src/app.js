42c745925d983efd22e063ef2d0ecf829279f1f7
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-20 02:06:36 +0530

    submit

--- src/app.js
+++ src/app.js
@@ -15,7 +15,39 @@ app.use(express.json());
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
 
+app.patch('/api/v1/products/:id', (req, res) => {
+    const proId=  req.params.id;  
+    let product= products.find((p)=> p.id == proId );
+    
+    if(product)
+    {
+        let quantityAvailable=parseInt(product.quantity);
+        if(quantityAvailable)
+        {
+             product={...product,quantity:quantityAvailable-1};
+             let updatedProducts= products.map((item)=>item.id==proId?product:item);
+             fs.writeFileSync(`${__dirname}/data/product.json`,JSON.stringify(updatedProducts));
+             return res.status(200).send({
+                    status: "success",
+                    message: `Thank you for purchasing Product ${proId}`,
+                    product: product
+             })
+        }   
+        else
+        {
+              return res.status(404).send(
+                { status: "success", message: `Product ${proId}, Out of stock!`, }
+              )
+        }  
+    }
+    else
+    {
+       return res.status(404).send(
+            { status: "failed", message: "Product not found!" } 
 
+        )
+    }
+})
 
 
 module.exports = app;
